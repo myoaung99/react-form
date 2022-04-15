c22a6a53e5f1679d64f157681d5d905f15d72a0f
MyoMyintAung <myomyintaung064@gmail.com>
2022-04-15 20:44:02 +0630

    created basic login form with custom hook validation

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,100 @@
+import useInput from "../hooks/useInput";
+
 const BasicForm = (props) => {
+  const {
+    value: firstName,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    valueBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+    inputClasses: firstNameClasses,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: lastName,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    valueBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+    inputClasses: lastNameClasses,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    valueBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+    inputClasses: emailClasses,
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+
+  // for all inputs
+  if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!firstNameIsValid) {
+      return;
+    }
+
+    console.log(firstName, lastName, email);
+
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="control-group">
+        <div className={firstNameClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={firstName}
+            onChange={firstNameChangeHandler}
+            onBlurCapture={firstNameBlurHandler}
+          />
+          {firstNameHasError && (
+            <p className="error-text">First Name must not empty.</p>
+          )}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lastNameClasses}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            value={lastName}
+            type="text"
+            id="name"
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameHasError && (
+            <p className="error-text">Last Name must not empty.</p>
+          )}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailClasses}>
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="email"
+          id="name"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={email}
+        />
+        {emailHasError && <p className="error-text">E-mail is not correct.</p>}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,34 +1,71 @@
+// import { useState } from "react";
+
+// const useInput = (validate) => {
+//   const [enterValue, setEnterValue] = useState("");
+//   const [isTouched, setIsTouched] = useState(false);
+
+//   // component mount တာနဲ့ validate တန်းလုပ်တော့ isValid က error တန်းထွက်တယ်
+//   const valueIsValid = validate(enterValue);
+//   // ဒါမဲ့ user က input field ကို touch လုပ်မထားရသေး(စ ဝင်ဝင်ချင်မို့)လို့ isInvalid က false နေတာ
+//   const hasError = isTouched && !valueIsValid;
+
+//   const valueChangeHandler = (event) => {
+//     setEnterValue(event.target.value);
+//   };
+
+//   const valueBlurHandler = () => {
+//     setIsTouched(true);
+//   };
+
+//   const resetForm = () => {
+//     setEnterValue("");
+//     isTouched(false);
+//   };
+
+//   return {
+//     value: enterValue,
+//     hasError,
+//     valueIsValid,
+//     valueChangeHandler,
+//     valueBlurHandler,
+//     resetForm,
+//   };
+// };
+
+// export default useInput;
+
 import { useState } from "react";
 
 const useInput = (validate) => {
-  const [enterValue, setEnterValue] = useState("");
+  const [enteredValue, setEnteredValue] = useState("");
   const [isTouched, setIsTouched] = useState(false);
 
-  // component mount တာနဲ့ validate တန်းလုပ်တော့ isValid က error တန်းထွက်တယ်
-  const valueIsValid = validate(enterValue);
-  // ဒါမဲ့ user က input field ကို touch လုပ်မထားရသေး(စ ဝင်ဝင်ချင်မို့)လို့ isInvalid က false နေတာ
+  const valueIsValid = validate(enteredValue);
   const hasError = isTouched && !valueIsValid;
 
   const valueChangeHandler = (event) => {
-    setEnterValue(event.target.value);
+    setEnteredValue(event.target.value);
   };
 
   const valueBlurHandler = () => {
     setIsTouched(true);
   };
 
-  const resetForm = () => {
-    setEnterValue("");
-    isTouched(false);
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
   };
 
+  const inputClasses = hasError ? "form-control invalid" : "form-control";
+
   return {
-    value: enterValue,
+    value: enteredValue,
+    isValid: valueIsValid,
     hasError,
-    valueIsValid,
     valueChangeHandler,
     valueBlurHandler,
-    resetForm,
+    reset,
+    inputClasses,
   };
 };
 
